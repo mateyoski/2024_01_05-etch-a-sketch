08511cd227f7b72eba4c09ea6f57ffa4ca644b40
mateyoski <maciejnedza01@gmail.com>
2024-03-30 11:54:06 +0100

    Added slider (not functioning yet)

--- script.js
+++ script.js
@@ -1,14 +1,28 @@
 const canvas = document.getElementById('canvas');
+const resetButton = document.getElementById('reset');
+
 function createGrid () {
-    for( let i = 0; i > 15; i++) {
+    for( let i = 0; i < 16; i++) {
         const row = document.createElement("div");
         row.classList.add("row");
-        for( let j = 0; j < 15; j++) {
+        for( let j = 0; j < 16; j++) {
             const square = document.createElement("div");
             square.classList.add("square");
             square.style.border = "1px solid black"
+
+            square.addEventListener('mouseover', function(){
+                square.style.backgroundColor = 'black';
+            })
+
             row.appendChild(square);
         }
         canvas.appendChild(row);
     }
 }
+
+createGrid();
+
+resetButton.addEventListener('click', function () {
+    canvas.innerHTML = "";
+    createGrid();
+})

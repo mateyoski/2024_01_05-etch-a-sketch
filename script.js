96c1a8b2e4ca8f0d6da4f6cef534d6c81f365578
mateyoski <maciejnedza01@gmail.com>
2024-04-08 11:12:11 +0200

    Finished functionalities: slider with displayable range, changing grid size dynamically

--- script.js
+++ script.js
@@ -1,11 +1,15 @@
+const DEFAULT_SIZE = 16;
+
 const canvas = document.getElementById('canvas');
 const resetButton = document.getElementById('reset');
+const sizeSlider = document.getElementById('sizeSlider');
+const sizeValue = document.getElementById('sizeValue');
 
-function createGrid () {
-    for( let i = 0; i < 16; i++) {
+function createGrid (size) {
+    for( let i = 0; i < size; i++) {
         const row = document.createElement("div");
         row.classList.add("row");
-        for( let j = 0; j < 16; j++) {
+        for( let j = 0; j < size; j++) {
             const square = document.createElement("div");
             square.classList.add("square");
             square.style.border = "1px solid black"
@@ -20,9 +24,23 @@ function createGrid () {
     }
 }
 
-createGrid();
 
 resetButton.addEventListener('click', function () {
     canvas.innerHTML = "";
-    createGrid();
+    createGrid(sizeSlider.value);
 })
+
+sizeSlider.addEventListener("input", function() {
+    canvas.innerHTML = "";
+    createGrid(sizeSlider.value);
+    sizeSlider.innerHTML = sizeSlider.value;
+    updateSizeValue(sizeSlider.value);
+})
+
+window.onload = () => {
+    createGrid(DEFAULT_SIZE)
+}
+
+function updateSizeValue(value) {
+    sizeValue.innerHTML = `${value} x ${value}`
+  }
